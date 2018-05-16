65776fbc82fa905504207d09f50221ad419fb838
Tjerk Woudsma <tjerk@studiomoniker.com>
2018-05-17 00:39:21 +0200

    cleanup

--- src/reducers/app.js
+++ src/reducers/app.js
@@ -12,7 +12,6 @@ export default (state = initialState, action) => {
       return {
         ...state,
         initialized: true,
-        transitions: true,
       }
 
     case constants.RESET_APP:
